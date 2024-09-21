9609bfbf234a730426e9bc60fc4263401bd82532
zo <rvyeinq@gmail.com>
2024-09-21 17:46:49 -0500

    Update main.cpp

--- BalloonGame/main.cpp
+++ BalloonGame/main.cpp
@@ -4,51 +4,57 @@
 //#include <stdio.h>
 #include <iostream>
 
-#include <windows.h>
+// Sleep(500);
+#include <Windows.h>
 #include <synchapi.h>
 
 int main()
 {
-    // Main window, load image
-    sf::RenderWindow window(sf::VideoMode(200,200), "Game");
-    sf::Texture bt;
-    if (!bt.loadFromFile("balloon.png"))
+    int i = 0;
+
+    sf::RenderWindow MainWindow(sf::VideoMode(800,800), "Game");
+    MainWindow.setFramerateLimit(60);
+    
+    sf::Texture texture;
+    if (!texture.loadFromFile("balloon.png"))
         return EXIT_FAILURE;
-    sf::Sprite bs(bt);
+    sf::Sprite BalloonSprite(texture);
 
-    bs.setScale(0.25, 0.25);
+    BalloonSprite.setScale(0.25, 0.25);
+    BalloonSprite.setPosition(0, 0);
 
+    int MousePositionY = sf::Mouse::getPosition().y;
+    int MousePositionX = sf::Mouse::getPosition().x;
     //mX = sf::Mouse::getPosition().x;
 
     // Main loop
-
-    while (window.isOpen())
-    {
-        // SetTimer(NULL, 0, 1000, NULL);
-        Sleep(500);
-    
-
-        // printf("%s\n", sf::Mouse::getPosition().x);
-        // printf("%s\n", sf::Mouse::getPosition().y);
-    
-        std::cout << (sf::Mouse::getPosition().x);
-        std::cout << "\n";
-        std::cout << (sf::Mouse::getPosition().y);
-        std::cout << "\n";
-    }
-
-    while (window.isOpen())
+    while (MainWindow.isOpen())
     {
         sf::Event event;
-        while (window.pollEvent(event))
+        while (MainWindow.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
-                window.close();
+                MainWindow.close();
         }
+        switch(event.type ==)
+        
 
-        window.clear();
-        window.draw(bs);
-        window.display();
+        MainWindow.clear();
+        MainWindow.draw(BalloonSprite);
+        BalloonSprite.move(float(MousePositionX), float(MousePositionY));
+        MainWindow.display();
+
+        // Slow down
+        i += 1;
+
+        if (i == 20) {
+            std::cout << (sf::Mouse::getPosition().x);
+            std::cout << "\n";
+            std::cout << (sf::Mouse::getPosition().y);
+            std::cout << "\n";
+
+            i -= 20;
+        }
     }
 
     return 0;
