dc8e14782c0afac47e9aacd0d94b01163f17983b
allie <rvyeinq@gmail.com>
2024-09-22 20:00:49 -0500

    Update balloon.h

--- BalloonGame/balloon.h
+++ BalloonGame/balloon.h
@@ -1,12 +1,27 @@
 #pragma once
 
-#ifndef BALLOON_H
-#define BALLOON_H
+#ifndef BLOON_H
+#define BLOON_H
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 
-sf::Sprite createSmallBalloon(int x, int y);
 bool CheckSpriteCollision(sf::Sprite s, sf::RenderWindow& w);
+
+class Bloon {
+	private:
+		int b_Health;
+		int b_Speed;
+		bool b_isDead;
+
+		Vector2f b_Position;
+		Texture b_Texture;
+
+	public:
+		Sprite spawn();
+		void kill();
+		int getHealth();
+		Sprite spawn(int x, int y);
+};
 
 #endif
