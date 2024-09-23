26b32ca9cdc542dd28ed416779eaa7f5ee587812
allie <rvyeinq@gmail.com>
2024-09-23 01:27:19 -0500

    Update main.cpp

--- BalloonGame/main.cpp
+++ BalloonGame/main.cpp
@@ -9,27 +9,31 @@
 using namespace sf;
 
 int main() {
+    // Game states, used for determining what is drawn onscreen
     enum class State {
         MENU, INGAME, PAUSE, END };
     State state = State::MENU;
+
+    // display resolution, used as window size
     Vector2f DisplayResolution;
     DisplayResolution.x =
         VideoMode::getDesktopMode().width / 2;
     DisplayResolution.y =
         VideoMode::getDesktopMode().height / 2;
 
-    int i = 0;
-
+    // main window, 60 fps limit
     RenderWindow MainWindow(
         VideoMode(DisplayResolution.x, DisplayResolution.y),
         "Game");
     MainWindow.setFramerateLimit(60);
-
+    
+	// used to store bloons onscreen
     std::vector<Bloon> bloons;
     Bloon balloon;
     balloon.spawn(5, 5);
     bloons.push_back(balloon);
 
+	// main game loop
     while (MainWindow.isOpen())
     {
         Event event;
@@ -40,19 +44,17 @@ int main() {
                 MainWindow.close();
             case Event::MouseButtonPressed:
                 if (event.mouseButton.button == Mouse::Left) {
-                    if (event.mouseButton.button == Mouse::Left) {
-                        for (auto& b : bloons) {
-                            if (CheckSpriteCollision(b.getSprite(), MainWindow)) {
-                                b.hit();
-                                std::cout << "hit!\n";
-                                std::cout << b.isDead() << "\n";
-                            }
+                    for (auto& b : bloons) {
+                        if (CheckSpriteCollision(b.getSprite(), MainWindow)) {
+                            b.hit();
+                            std::cout << "hit!\n";
+                            std::cout << b.isDead() << "\n";
                         }
                     }
                 }
                 MainWindow.clear();
-                // only draw sprite if it is alive
 
+				// check if a sprite is "alive" before drawing it
                 for (auto it = bloons.begin(); it != bloons.end();) {
                     if (it->isDead()) {
                         it = bloons.erase(it);
