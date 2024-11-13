b6814ec6fad019a78398fd5dbcdc1a2ecc056c38
pincx <rvyeinq@gmail.com>
2024-11-12 23:38:28 -0600

    useless

--- BalloonGame/bloon.cpp
+++ BalloonGame/bloon.cpp
@@ -8,7 +8,6 @@ bool CheckSpriteCollision(Sprite s, RenderWindow& w) {
 	if (Mouse::isButtonPressed(Mouse::Button::Left)) {
 		Vector2i MousePosition = Mouse::getPosition(w);
 		Vector2f SpritePosition = s.getPosition();
-		Vector2f SpriteSize = s.getScale();
 
 		if (MousePosition.x >= SpritePosition.x &&
 			MousePosition.x <= (SpritePosition.x + s.getGlobalBounds().width) &&
